64a9f28cfda669120f37152de5151e47c45735f3
Pierre Le Page <pierre.lepage.44@gmail.com>
2020-07-01 17:00:36 +0200

    Config PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoxesController } from './boxes/boxes.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'ca-nestjs',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [BoxesController],
   providers: [AppService],
 })
