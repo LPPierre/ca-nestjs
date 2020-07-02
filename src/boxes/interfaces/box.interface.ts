522089b21b14faeba0812b7303348d8b30834d20
Pierre Le Page <pierre.lepage.44@gmail.com>
2020-07-02 14:36:21 +0200

    Remove unused interfaces

--- src/boxes/interfaces/box.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface Box {
-    label: string;
-}
--- src/creatures/interfaces/creature.interface.ts
+++
@@ -1,8 +0,0 @@
-import { CreatureType } from '../creatureType.enum';
-import { Box } from 'src/boxes/box.entity';
-
-export interface Creature {
-    name: string;
-    type: CreatureType;
-    box: Box;
-}
